c5298ffe6ea0b2d63569f326f8412667aee75ed4
Matheus lima <matheusslimabm@gmail.com>
2024-09-20 14:56:17 -0300

    summary

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,5 +1,41 @@
+import { Plus } from 'lucide-react'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import { InOrbitIcon } from './ui/in-orbit-icon'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+
+const style = { width: '50%' }
+
 export function Summary() {
-    return (
-        <h1>metas do usuario</h1>
-    )
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de Agosto</span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator style={style} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">8</span> de{' '}
+            <span className="text-zinc-100">15</span> metas nessa semana.
+          </span>
+          <span>58%</span>
+        </div>
+      </div>
+    </div>
+  )
 }
