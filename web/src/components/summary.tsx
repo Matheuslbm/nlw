6d0a9b3a2caa04acfc068284f1613e3254839f00
Matheus lima <matheusslimabm@gmail.com>
2024-09-20 20:03:24 -0300

    fim aula 2

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,8 +1,10 @@
-import { Plus } from 'lucide-react'
+import { CheckCircle2, Plus } from 'lucide-react'
 import { Button } from './ui/button'
 import { DialogTrigger } from './ui/dialog'
 import { InOrbitIcon } from './ui/in-orbit-icon'
 import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
+import { OutlineButton } from './ui/outline-button'
 
 const style = { width: '50%' }
 
@@ -34,6 +36,90 @@ export function Summary() {
             <span className="text-zinc-100">15</span> metas nessa semana.
           </span>
           <span>58%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex gap-3 flex-wrap">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Nadar
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Praticar exercício
+        </OutlineButton>
+
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Estudar
+        </OutlineButton>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-medium">Sua semana</h2>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Domingo{' '}
+            <span className="text-zinc-400 text-xs">(15 de setembro)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{' '}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{' '}
+                <span className="text-zinc-100">08:13</span>
+              </span>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{' '}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{' '}
+                <span className="text-zinc-100">08:13</span>
+              </span>
+            </li>
+
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{' '}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{' '}
+                <span className="text-zinc-100">08:13</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Segunda-feira{' '}
+            <span className="text-zinc-400 text-xs">(16 de setembro)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou{' '}
+                <span className="text-zinc-100">"Acordar cedo"</span> às{' '}
+                <span className="text-zinc-100">08:13</span>
+              </span>
+            </li>
+
+        
+          </ul>
         </div>
       </div>
     </div>
